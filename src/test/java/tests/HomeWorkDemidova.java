864c4377b256f0af6eb2e4f39b0a7e5cee8597be
demidova.ln <lee8676@yandex.ru>
2022-11-15 21:41:10 +0300

    Second commit. Ex13:User Agent

--- src/test/java/tests/HomeWorkDemidova.java
+++ src/test/java/tests/HomeWorkDemidova.java
@@ -1,71 +1,48 @@
-package tests;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-        import io.restassured.RestAssured;
-        import io.restassured.response.Response;
-        import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import java.util.Map;
 
-        import java.io.IOException;
-        import java.net.URL;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HomeWorkDemidova {
 
-    @Test
-    public void testRestAssured() throws IOException {
+    @ParameterizedTest
+    @CsvSource(value = {"'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30', Mobile, No, Android",
+             "'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1', Mobile, Chrome, iOS",
+            "'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)', Googlebot, Unknown, Unknown",
+            "'Mozilla/5.0 ('Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0', Web, Chrome, No",
+            "'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1', Mobile, No, iPhone"}, delimiter = ',')
 
-        Response responseFirstRedrect = RestAssured
+    public void testUserAgentTest (String userAgent, String expectedPlatform, String expectedBrowser, String expectedDevice){
+        Map<String, String> headersParam = new HashMap<>();
+        headersParam.put("User-Agent", userAgent);
+
+        JsonPath response = RestAssured
                 .given()
-                .redirects()
-                .follow(false)
-                .when()
-                .get("https://playground.learnqa.ru/api/long_redirect")
-                .andReturn();
+                .header("User-Agent", userAgent)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .jsonPath();
 
-        int statusCode = responseFirstRedrect.getStatusCode();
-        System.out.println(statusCode);
-        String url = responseFirstRedrect.getHeader("Location");
-        System.out.println(url);
+        //response.prettyPrint();
+       String receivedPlatform = response.getString("platform");
+//      System.out.println(receivedPlatform);
+        String receivedBrowser = response.getString("browser");
+//      System.out.println(receivedBrowser);
+        String receivedDevice = response.getString("device");
+//      System.out.println(receivedDevice);
 
-        URL obj = new URL(url);
-        Response responseSecondRedrect = RestAssured
-                .given()
-                .redirects()
-                .follow(false)
-                .when()
-                .get(obj)
-                .andReturn();
+        assertEquals(expectedPlatform, receivedPlatform, "Platforms don't match");
+        assertEquals(expectedBrowser, receivedBrowser, "Browsers don't match");
+        assertEquals(expectedDevice, receivedDevice, "Devices don't match");
 
-        statusCode = responseSecondRedrect.getStatusCode();
-        System.out.println(statusCode);
-        String url2 = responseSecondRedrect.getHeader("Location");
-        System.out.println(url2);
+     }
 
-        obj = new URL(url2);
-        Response responseThirdRedrect = RestAssured
-                .given()
-                .redirects()
-                .follow(false)
-                .when()
-                .get(obj)
-                .andReturn();
-
-        statusCode = responseThirdRedrect.getStatusCode();
-        System.out.println(statusCode);
-        String url3 = responseThirdRedrect.getHeader("Location");
-        System.out.println(url3);
-
-        obj = new URL(url3);
-        Response responseFourthRedrect = RestAssured
-                .given()
-                .redirects()
-                .follow(false)
-                .when()
-                .get(obj)
-                .andReturn();
-
-        statusCode = responseFourthRedrect.getStatusCode();
-        System.out.println(statusCode);
-        String url4 = responseFourthRedrect.getHeader("Location");
-        System.out.println(url4);
-    }
 }
+
